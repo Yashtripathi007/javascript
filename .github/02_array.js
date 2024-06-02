2b8c330241b378205725989ed42ab88d25b93beb
yash <105648218+Yashtripathi007@users.noreply.github.com>
2024-06-02 22:40:36 +0000

    arraychanges

---
+++ .github/02_array.js
@@ -0,0 +1,13 @@
+//there are lots of operations on array where you can manupulate your array let me show u 
+
+const arr=[1,2,3,4,5];
+//console.log(arr);
+const myarr= arr.join();
+//console.log(myarr);
+console.log(arr.length);
+console.log(arr.slice(1,3));
+console.log(arr.includes(19));
+
+console.log(myarr);
+
+
